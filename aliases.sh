45b9fe8d04a08e6aeff352bbff12fcba563fffc1
Andrew Ma <anma@scires.com>
2021-07-14 15:42:14 -0500

    Add aliases.sh file that can be sourced

---
+++ aliases.sh
@@ -0,0 +1,20 @@
+##### Aliases #####
+alias ciphey="docker run -it --rm remnux/ciphey"
+
+alias steg="docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v \"$(pwd)/data:/data\" dominicbreuker/stego-toolkit /bin/bash"
+
+alias bw="binwalk -e -D=\".*\""
+
+alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
+
+
+##### Environment Variables ####
+export PATH="hot:$PATH"
+
+
+##### Commands ####
+# Make CAPS to ESC
+setxkbmap -option "caps:swapescape"
+
+# Make display work for 'steg' alias
+xhost +"local:docker@" > /dev/null
