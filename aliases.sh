abe3b483f84f4ac6e4a7fd01eeb826d671eb73be
andrew-ma <andrew-ma@users.noreply.github.com>
2021-07-15 01:58:00 -0400

    Make b64 function to decode base64 faster, and rename python version to b64py

--- aliases.sh
+++ aliases.sh
@@ -8,13 +8,22 @@ alias bw="binwalk -e -D=\".*\""
 alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
 
 
-##### Environment Variables ####
+##### Environment Variables #####
 # Add current file's path to PATH
 export PATH="$(realpath $(dirname $0)):$PATH"
 
-##### Commands ####
+
+##### Functions #####
+# Use string on right side with 'base64' command
+# Defaults to decode string
+# Encodes if "-e"
+b64 () { if [ "$1" = "-d" ]; then decFlag="-d"; shift; elif [ "$1" = "-e" ]; then decFlag=""; else decFlag="-d"; fi; if [ -z "$1" ]; then echo "b64 [-e/-d] string"; return; fi; arg1="$1"; shift; echo "$arg1" | base64 $decFlag "$@"}
+
+
+##### Commands #####
 # Make CAPS to ESC
 setxkbmap -option "caps:swapescape"
 
 # Make display work for 'steg' alias
 xhost +"local:docker@" > /dev/null
+
