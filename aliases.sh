22480ebfba89dac2db8f6ab89bdd75b856dd9472
andrew-ma <andrew-ma@users.noreply.github.com>
2021-07-15 05:06:11 -0400

    Fix adding to path bug and steg command not getting current directory only at alias time

--- aliases.sh
+++ aliases.sh
@@ -3,17 +3,19 @@ alias ciphey="docker run -it --rm remnux/ciphey"
 
 alias killCiphey="kill -9 `ps aux | grep 'remnux/ciphey' | head -n1 | awk '{print $2}'`"
 
-alias steg="docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v \"$(pwd)/data:/data\" dominicbreuker/stego-toolkit /bin/bash"
-
 alias bw="binwalk -e -D=\".*\""
 
 alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
 
 
 ##### Environment Variables #####
-# Add current file's path to PATH if it is not found in PATH already
-script_dir=$(realpath $(dirname $0))
-[[ "$PATH" =~ $script_dir ]] || export PATH="$script_dir:$PATH"
+# Add current file's directory to PATH if it is not found in PATH already
+SCRIPT_DIR=$(realpath $(dirname $0))
+CUR_DIR="$PWD"
+#SCRIPT_FILE="${CUR_DIR}/${0#*/}"
+
+if [[ "$PATH" == *"$SCRIPT_DIR"* ]]; then : ; else export PATH="$SCRIPT_DIR:$PATH"; fi
+if [[ "$PATH" == *"$CUR_DIR"* ]]; then : ; else export PATH="$CUR_DIR:$PATH"; fi
 
 
 ##### Functions #####
@@ -24,12 +26,14 @@ b64 () { if [ "$1" = "-d" ]; then decFlag="-d"; shift; elif [ "$1" = "-e" ]; the
 
 b64py () { if [ -z "$1" ]; then echo "Usage: $0 <base64String>"; return 1; fi; echo "You entered: '$1'"; python3 -c "import base64; x=base64.b64decode('$1'); print(x.decode())"; }
 
+steg () { [ ! -d "/path/to/dir" ] && mkdir -p data && docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v "$(pwd)/data:/data" dominicbreuker/stego-toolkit /bin/bash; }
+
 
 ##### Commands #####
 # Make CAPS to ESC
 # setxkbmap -option "caps:swapescape"
 
 # Make display work for 'steg' alias
-xhost +"local:docker@" > /dev/null
+xhost +"local:docker@" > /dev/null 2>&1
 
 echo "Sourced \"$0\" successfully"
